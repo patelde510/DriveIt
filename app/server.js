2c8564a5206011e016c3f030cd1f20d7535c3bfc
rickypatel5112 <rickypatel5112@gmail.com>
2024-11-02 20:06:28 -0400

    Starter code for server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,14 @@
 // TODO
+let express = require("express");
+let app = express();
+let apiFile = require("../env.json");
+let port = 3000;
+let hostname = "localhost";
+
+app.get("/", (req, res) => {
+    res.send('Hello World!');
+  })
+
+app.listen(port, hostname, () => {
+    console.log(`http://${hostname}:${port}`);
+});
