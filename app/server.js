59b17114c29b969dd39fca56a4a9953d04e87ec3
devpatel <dkp55@drexel.edu>
2024-11-07 15:50:13 -0500

    Fix deploymentt

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@
 let express = require("express");
 let app = express();
 let port = 8080;
-let hostname = 'localhost'; // for local testing change this to localhost --'0.0.0.0'
+let hostname = '0.0.0.0'; // for local testing change this to localhost --'0.0.0.0'
 
 app.use(express.static(__dirname));
 
